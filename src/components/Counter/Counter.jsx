230b4ad8a431eeb91aba86e9ad74247be7d72893
RazielBlaster <jhojanblackout@gmail.com>
2023-10-29 09:53:09 -0500

    Add pause btn

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -7,59 +7,59 @@ import rest from '../../assets/rest.mp3'
 
 const Counter = ({ time, timeWork, timeRest, title }) => {
 
-    
-    const { rounds } = useOptions()
-    const { setIsWorkFalse } = useOptions()
-    const { setIsWorkTrue } = useOptions()
-    const { setIsRestFalse } = useOptions()
-    const { setIsRestTrue } = useOptions()
-    const { setRoundsMinus } = useOptions()
-    const { isWork } = useOptions()
-    const { isRest } = useOptions()
-    const { setStartFalse } = useOptions()
-    
-    
-    
+
+    const { rounds, 
+        setIsWorkFalse, 
+        setIsWorkTrue, 
+        setIsRestFalse, 
+        setIsRestTrue, 
+        setRoundsMinus, 
+        isWork, 
+        isRest, 
+        setStartFalse } = useOptions();
 
     const [seconds, setSeconds] = useState(time);
-    
-    useEffect(() => {
-        if (rounds > 0) {
+    const [isPaused, setIsPaused] = useState(false);
+    const [isFinish, setIsFinish] = useState(false);
 
-            const counter = setInterval(() => {
+    useEffect(() => {
+        let counter;
+
+        if (rounds > 0 && !isPaused) {
+            counter = setInterval(() => {
                 setSeconds((prevSeconds) => {
                     const newSeconds = prevSeconds - 1;
 
                     if (newSeconds <= 0) {
-
                         clearInterval(counter);
                         if (isWork) {
-                            setIsRestTrue()
-                            setIsWorkFalse()
+                            setIsRestTrue();
+                            setIsWorkFalse();
                         }
-
                         if (isRest) {
-                            let count = rounds - 1
-                            setRoundsMinus(count)
-                            setIsRestFalse()
-                            setIsWorkTrue()
+                            let count = rounds - 1;
+                            setRoundsMinus(count);
+                            setIsRestFalse();
+                            setIsWorkTrue();
                         }
                     }
-
                     return newSeconds;
                 });
             }, 1000);
-            
-            
-            return () => clearInterval(counter);
         }
-        setStartFalse()
-        setIsRestFalse()
-        setIsWorkFalse()
 
-    }, []);
+        if (rounds === 0) {
+            setIsRestFalse()
+            setIsWorkFalse()
+            setStartFalse()
+        }
 
+        return () => clearInterval(counter);
+    }, [isPaused]);
 
+    const handlePauseToggle = () => {
+        setIsPaused((prevIsPaused) => !prevIsPaused);
+    };
 
 
 
@@ -72,39 +72,46 @@ const Counter = ({ time, timeWork, timeRest, title }) => {
             <h1>{title}</h1>
 
             <section className="work__interface">
-                <h4>Rounds:    {rounds}   </h4>
+                <h4>Rounds: {rounds}</h4>
                 <h1>{seconds}</h1>
+                <button onClick={handlePauseToggle}>
+                    {isPaused ? 'Reanudar' : 'Pausar'}
+                </button>
             </section>
 
+
+
             {
+
                 isWork &&
-                <audio autoPlay>
-                    <source src= {bell} type="audio/mp3" />
-                    Tu navegador no soporta la reproducción de audio.
-                </audio>
+                <section>
+
+                    <audio autoPlay>
+                        <source src={bell} type="audio/mp3" />
+                        Tu navegador no soporta la reproducción de audio.
+                    </audio>
+
+
+
+                </section>
             }
 
             {
                 isRest &&
-                <audio autoPlay>
-                    <source src={rest} type="audio/mp3" />
-                    Tu navegador no soporta la reproducción de audio.
-                </audio>
+
+                <section>
+
+                    <audio autoPlay>
+                        <source src={rest} type="audio/mp3" />
+                        Tu navegador no soporta la reproducción de audio.
+                    </audio>
+
+                </section>
             }
 
-{/* 
-            {
-                finish && (
-                    <div>
-                    <h1>You did</h1>
-                <audio autoPlay>
-                    <source src="public\complete.mp3" type="audio/mp3" />
-                    Tu navegador no soporta la reproducción de audio.
-                </audio>
-                </div>
-                    )
-            }
- */}
+
+
+
 
 
         </div >
